802063e69df753465977b6fe3729aeb6b03ad2e8
cesar <solito_203@hhotmail.com>
2017-12-25 18:58:10 -0500

    ff

---
+++ curso7_Calculadora/curso7_Calculadora/curso7_Calculadora/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace curso7_Calculadora
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new curso7_Calculadora.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ curso7_Calculadora/curso7_Calculadora/curso7_Calculadora/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace curso7_Calculadora
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            pkOper.Items.Add("Suma");
+            pkOper.Items.Add("Resta");
+            pkOper.Items.Add("Multiplicacion");
+            pkOper.Items.Add("Division");
+        }
+
+        private void pkOper_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String op = pkOper.SelectedItem.ToString();
+            int n1 = Int32.Parse(txtN1.Text);
+            int n2 = Int32.Parse(txtN2.Text);
+            double res = 0;
+
+            if (op == "Suma")
+            {
+                res = n1 + n2;
+            }
+            else if (op == "Resta")
+            {
+                res = n1 - n2;
+            }
+            else if (op == "Multiplicacion")
+            {
+                res = n1 * n2;
+            }
+            else if (op == "Division")
+            {
+                res = n1 / n2;
+            }
+
+            lblResultado.Text = res + "";
+
+        }
+    }
+}
