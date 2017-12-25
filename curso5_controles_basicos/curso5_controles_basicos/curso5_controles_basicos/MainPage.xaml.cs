1eb1b0382fc8902c4fe0934afbdb4c7e766263f8
cesar <solito_203@hhotmail.com>
2017-12-25 18:42:59 -0500

    ff

---
+++ curso5_controles_basicos/curso5_controles_basicos/curso5_controles_basicos/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace curso5_controles_basicos
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+ 
+
+        private void btnMostrar_Clicked_1(object sender, EventArgs e)
+        {
+            lblResultado.Text = "Bienvenido al Sistema " + txtNom.Text;
+            txtNom.Text = "";
+
+
+        }
+    }
+}
