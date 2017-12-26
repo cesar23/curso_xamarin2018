aff2f6aba6f2549c57c036a1bded8e26e94a021d
cesar <solito_203@hhotmail.com>
2017-12-25 19:32:44 -0500

    ff

---
+++ curso9_DisplayAlert/curso9_DisplayAlert/curso9_DisplayAlert/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace curso9_DisplayAlert
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void btnMsg_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensaje", "Mensaje de prueba", "Cancelar");
+        }
+
+        private async void btnMsg2_Clicked(object sender, EventArgs e)
+        {
+            bool res = await DisplayAlert("Mensaje", "Cambiar color","Aceptar", "Cancelar");
+
+            if (res == true)
+                fondo.BackgroundColor = Color.Yellow;
+            else
+                fondo.BackgroundColor = Color.White;
+        }
+    }
+}
