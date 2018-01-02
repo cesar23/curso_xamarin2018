9a8ca5239b58dceacbb2f30f6e6604e996843e2d
cauris <cauris@peruapps.com.pe>
2018-01-02 15:00:20 -0500

    termino del curso xamarin

---
+++ cur16_TabsNav/cur16_TabsNav/cur16_TabsNav/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace cur16_TabsNav
+{
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
